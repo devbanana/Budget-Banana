08ee3b10a4cb07cdaf8285869aaea703fa1ac3be
Brandon Olivares <programmer2188@gmail.com>
2014-09-03 15:42:20 -0400

    Installed FOSJsRoutingBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new Devbanana\BudgetBundle\DevbananaBudgetBundle(),
         );
 
