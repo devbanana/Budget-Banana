db93f152386bd075bb19ec80bbe64c52e80eac44
Brandon Olivares <programmer2188@gmail.com>
2014-09-02 22:48:07 -0400

    Created Transaction form

---
+++ src/Devbanana/BudgetBundle/Form/TransactionType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Devbanana\BudgetBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TransactionType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date', 'date', array(
+                        'years' => range(date('Y'), date('Y')+1),
+                        ))
+            ->add('inflow')
+            ->add('outflow')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Devbanana\BudgetBundle\Entity\Transaction'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'devbanana_budgetbundle_transaction';
+    }
+}
