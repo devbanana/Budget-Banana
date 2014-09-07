123085e5296ac557708dfae3ce9e91a8f1390814
Brandon Olivares <programmer2188@gmail.com>
2014-09-07 09:30:31 -0400

    Added memo field to LineItem

---
+++ app/DoctrineMigrations/Version20140907092959.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20140907092959 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        
+        $this->addSql('ALTER TABLE LineItem ADD memo VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        
+        $this->addSql('ALTER TABLE LineItem DROP memo');
+    }
+}
