78be0686d98ced94be30d6048897d91375ee0fc6
Brandon Olivares <programmer2188@gmail.com>
2014-09-15 19:02:38 -0400

    Added ability to sort account transactions

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\AopBundle\JMSAopBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Devbanana\BudgetBundle\DevbananaBudgetBundle(),
             new Devbanana\UserBundle\DevbananaUserBundle(),
             new Devbanana\PagesBundle\DevbananaPagesBundle(),
