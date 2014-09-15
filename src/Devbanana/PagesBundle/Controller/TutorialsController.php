922b7ce130b9ca340feab3a0ea2bd5b2ef35195d
Brandon Olivares <programmer2188@gmail.com>
2014-09-15 15:52:51 -0400

    Created first chapter of tutorial for Budget Banana

--- src/Devbanana/PagesBundle/Controller/TutorialsController.php
+++ src/Devbanana/PagesBundle/Controller/TutorialsController.php
@@ -19,6 +19,69 @@ class TutorialsController extends Controller
     {
         return array(
                 // ...
-            );    }
+                );    }
+
+        /**
+         * @Route("/chapter-1", name="tutorials_1")
+         * @Template()
+         */
+        public function chapter1Action()
+        {
+            return array();
+        }
+
+        /**
+         * @Route("/chapter-1/section-1", name="tutorials_1_1")
+         * @Template()
+         */
+        public function chapter1Section1Action()
+        {
+            return array();
+        }
+
+        /**
+         * @Route("/chapter-1/section-2", name="tutorials_1_2")
+         * @Template()
+         */
+        public function chapter1Section2Action()
+        {
+            return array();
+        }
+
+        /**
+         * @Route("/chapter-1/section-3", name="tutorials_1_3")
+         * @Template()
+         */
+        public function chapter1Section3Action()
+        {
+            return array();
+        }
+
+        /**
+         * @Route("/chapter-1/section-4", name="tutorials_1_4")
+         * @Template()
+         */
+        public function chapter1Section4Action()
+        {
+            return array();
+        }
+
+        /**
+         * @Route("/chapter-2", name="tutorials_2")
+         * @Template()
+         */
+        public function chapter2Action()
+        {
+            return array();
+        }
+
+        /**
+         * @Route("/chapter-2/section-1", name="tutorials_2_1")
+         * @Template()
+         */
+        public function chapter2Section1Action()
+        {
+            return array();
+        }
 
 }
