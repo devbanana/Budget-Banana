f2ab7657e04ae3b5edc0152fb4fae0af72b126b8
Brandon Olivares <programmer2188@gmail.com>
2014-09-15 22:59:53 -0400

    Fixed Resources/public/js/account.js
    It was trying to parse the JSON result when this is already done automatically.

--- src/Devbanana/BudgetBundle/Resources/public/js/account.js
+++ src/Devbanana/BudgetBundle/Resources/public/js/account.js
@@ -6,7 +6,6 @@ id: $(this).val()}),
 method: "POST",
 success: function (result)
 {
-result = JSON.parse(result);
 $('#devbanana_budgetbundle_account_budgeted').val(result.budgeted);
 }
             });
