6b6efe03e6ef8da26fe83e990d5d22e5f4d5e85b
Brandon Olivares <programmer2188@gmail.com>
2014-09-16 23:05:43 -0400

    Created chapter 2 of the tutorial

--- src/Devbanana/PagesBundle/Controller/TutorialsController.php
+++ src/Devbanana/PagesBundle/Controller/TutorialsController.php
@@ -84,4 +84,59 @@ class TutorialsController extends Controller
             return array();
         }
 
+        /**
+         * @Route("/chapter-2/section-2", name="tutorials_2_2")
+         * @Template()
+         */
+        public function chapter2Section2Action()
+        {
+            return array();
+        }
+
+        /**
+         * @Route("/chapter-2/section-3",
+         *     name="tutorials_2_3")
+         * @Template()
+         */
+        public function chapter2Section3Action()
+        {
+            return array();
+        }
+
+        /**
+         * @Route("/chapter-2/section-4", name="tutorials_2_4")
+         * @Template()
+         */
+        public function chapter2Section4Action()
+        {
+            return array();
+        }
+
+        /**
+         * @Route("/chapter-3", name="tutorials_3")
+         * @Template()
+         */
+        public function chapter3Action()
+        {
+            return array();
+        }
+
+        /**
+         * @Route("/chapter-3/section-1", name="tutorials_3_1")
+         * @Template()
+         */
+        public function chapter3Section1Action()
+        {
+            return array();
+        }
+
+        /**
+         * @Route("/glossary", name="tutorials_glossary")
+         * @Template()
+         */
+        public function glossaryAction()
+        {
+            return array();
+        }
+
 }
